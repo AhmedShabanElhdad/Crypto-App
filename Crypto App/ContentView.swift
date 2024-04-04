dd4624894ab20a6d62bc94dd79b597303487f734
Ahmed Shaaban <ahmed.shaaban.elhdah@gmail.com>
2024-04-04 13:22:31 +0300

    Initial Commit

---
+++ Crypto App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto App
+//
+//  Created by ahmed shaban on 04.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto App/Crypto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_AppApp.swift
+//  Crypto App
+//
+//  Created by ahmed shaban on 04.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
